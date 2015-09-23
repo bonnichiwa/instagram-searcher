bc7fe16c277ef3df36a33397f7564218eefb2798
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-22 23:22:38 -0400

    gets the response object from instagram, but when used in ng-repeat won't show

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 angular.module('instaApp', [])
   .controller('instaCtrl', function($scope, $http) {
+
+  var tagResults = false;
+
   $scope.searchItem = function(tag) {
-    var url = "https://api.instagram.com/v1/tags/{tag}/media/recent";
+    var url = "https://api.instagram.com/v1/tags/{tag-name}/media/recent";
     var request = {
       client_id: "f716627148d7477890ca3e35d2ce5d26",
-      tags: tag,
+      count: 10,
       callback: "JSON_CALLBACK"
     };
 
     $http({
       method: 'JSONP',
-      url: url,
+      url: url.replace("{tag-name}", $scope.tag),
       params: request
     })
     .then(function(response) {
       $scope.results = response.data;
+      console.log(response.data);
     },
     function(result) {
       alert('error');
